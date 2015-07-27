d20bf00c89022037d234b39e061168a803f39f57
Taylor Miley <coachtaylormiley@gmail.com>
2015-07-27 10:56:15 -0500

    Minor changes, added a json file for the first time

--- js-101.js
+++ js-101.js
@@ -1,20 +1,22 @@
-var songs = [];
+// var songs = [];
 
 
-  songs[songs.length] = "One > by Metallica on the album ...And Justice for All";
-  songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-  songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-  songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-  songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-  songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-  songs[songs.length] = "Nightcall > Kavinsky on the album Outrun";
+//   songs[songs.length] = "One > by Metallica on the album ...And Justice for All";
+//   songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+//   songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+//   songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+//   songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+//   songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+//   songs[songs.length] = "Nightcall > Kavinsky on the album Outrun";
+  
 
 
 
-for (i = 0; i < songs.length; i++) {
-	songs[i] = songs[i].replace(/>/g, "-");
-	songs[i] = songs[i].replace(/\*|@|\(|!/g, "");
-	container.innerHTML += songs[i] + "<br>" + "<br>" + " ";
-}
-console.log(songs);
+// for (i = 0; i < songs.length; i++) {
+// 	songs[i] = songs[i].replace(/>/g, "-");
+// 	songs[i] = songs[i].replace(/\*|@|\(|!/g, "");
+// 	songs[i] = "<div>" + songs[i] + "<div>";
+// 	container.innerHTML += songs[i] + "<br>" + "<br>" + " ";
+// }
+// console.log(songs);
 
